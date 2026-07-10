1379c0891462c1ea0119719fdaccb418c3d1a9d5
Salman Butt <salman0butt@gmail.com>
2026-07-10 17:04:02 +0500

    Fix import paths by adding file extensions for consistency

--- api/places.ts
+++ api/places.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { placeSuggestions } from './_duffel'
+import { placeSuggestions } from './_duffel.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
--- api/search.ts
+++ api/search.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { searchOffers } from './_duffel'
+import { searchOffers } from './_duffel.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
